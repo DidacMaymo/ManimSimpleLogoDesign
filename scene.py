78a29e2a2d2c52dc5d6a9540c53aed1840d634b7
Dídac Maymó <dmaymo@edu.tecnocampus.cat>
2022-12-07 17:33:30 +0100

    py code

---
+++ scene.py
@@ -0,0 +1,15 @@
+from Manim import *
+
+class logo(Scene):
+    def construct(self):
+        circle = Circle().scale(2)
+        circle.set_stroke(GREEN, width=0.5)
+        d= Text('D',slant=ITALIC, height=3)
+        self.play(Create(circle), run_time=2)
+        self.play(ReplacementTransform(circle,d))
+        self.play(d.animate.shift(2*LEFT))
+        source = Text('ídac Maymó', font_size=50, line_spacing=1).shift(DOWN-0.3).shift(RIGHT*1.4)
+        self.play(Write(source))
+        self.wait()
+        self.play(Unwrite(source, reverse=False),FadeOut(d, shift=DOWN * 2, scale=1.5))
+        self.wait()
